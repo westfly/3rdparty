9f8c0b1d2d63e705ffc00510c06fcb295d1f584d
aresyang <yweiyun@163.com>
2021-06-24 16:12:48 +0800

    updatecode

--- submodules/test/test_fmt.cpp
+++ submodules/test/test_fmt.cpp
@@ -3,7 +3,15 @@
 #include "fmt/printf.h"
 int main(int argc, const char *argv[])
 {
-    fmt::printf("Hello, %s!", "world");
+    fmt::printf("Hello, %s!\n", "world");
+    fmt::memory_buffer out;
+    std::string out_str("score");
+    for(int i = 0; i < 10; ++i) {
+        fmt::format_to(std::back_inserter(out_str), "The answer is {}.\n", i);
+        fmt::format_to(out, "The answer is {}.\n", i);
+    }
+    fmt::print("addd_string {}\n", out_str);
+    fmt::print("addd {}\n", fmt::to_string(out));
     return 0;
 }
 
