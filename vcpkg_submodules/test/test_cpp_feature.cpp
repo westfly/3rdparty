3fc28f08ba10ab48bda910202f2ac5594431ca10
aresyang <yweiyun@163.com>
2021-06-24 16:07:06 +0800

    update code

--- vcpkg_submodules/test/test_cpp_feature.cpp
+++ vcpkg_submodules/test/test_cpp_feature.cpp
@@ -2,17 +2,19 @@
 #include <variant>
 #include <string>
 #include <cassert>
+#include <fmt/format.h>
 
 void test_variant() {
     std::variant<int, float> w;
     w = 12;
     try {
-      std::get<float>(w);
+      fmt::print("{}\n", std::get<int>(w));
     }
     catch (const std::bad_variant_access&) {
+      fmt::print("error\n");
     }
 }
 
 int main(int argc, char* argv[]) {
-
+  test_variant();
 }
--- vcpkg_submodules/test/test_dataframe.cpp
+++ vcpkg_submodules/test/test_dataframe.cpp
@@ -52,7 +52,8 @@ struct Table {
                 continue;
             }
             bool big_data_set = false;
-            if (big_data_set || field_list[0].find("48bc47c808ad437a3f6e5cf0e2c103ee") !=
+            if (big_data_set ||
+                field_list[0].find("48bc47c808ad437a3f6e5cf0e2c103ee") !=
                     std::string_view::npos) {
                 fmt::print("new field {}\n", field_list[3]);
                 std::string id(field_list[0]);
@@ -92,7 +93,269 @@ private:
     std::vector<std::string> json_str_list;
     std::vector<Record>      records;
 };
+
 using namespace hmdf;
+
+template <typename I, typename H>
+int extractTopNFea(DataFrame<I, H>& df, std::string type = "Imp") {
+    auto df_size = df.get_index().size();
+    auto uniq_pkg_vec = df.template get_col_unique_values<std::string>("pkg");
+    int  mode_len = (int)uniq_pkg_vec.size();
+    std::cout << "in func: extractTopNFea, " << "df_zize=" << df_size << ", mode_len=" << mode_len << std::endl;
+    ModeVisitor<3, std::string> mode_visit;
+    const auto&                 result =
+        df.template single_act_visit<std::string>("pkg", mode_visit)
+            .get_result();
+    std::cout << "topNFeaof" << type << ":";
+    for (auto& i : result) {
+        std::cout << i.get_value() << ",";
+    }
+    if (mode_len == 2) {
+        ModeVisitor<3, std::string> mode_visit1;
+        const auto&                 result1 =
+            df.template single_act_visit<std::string>("pkg", mode_visit1)
+                .get_result();
+        for (auto& i : result) {
+            std::cout << i.get_value() << ",";
+        }
+    }
+    if (mode_len >= 3) {
+        ModeVisitor<60, std::string> mode_visit2;
+        const auto&                 result2 =
+            df.template single_act_visit<std::string>("pkg", mode_visit2)
+                .get_result();
+        for (auto& i : result) {
+            std::cout << i.get_value() << ",";
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+template <typename I, typename H>
+void extractTopNFeaByTime(DataFrame<I, H>& df, long req_ts) {
+    int  count = 0;
+    auto functor = [&count, req_ts](const unsigned long&,
+                                    const long& val) -> bool {
+        return (req_ts + count++) % 2 == 0;
+    };
+    auto df_24h =
+        df.template get_data_by_sel<long, decltype(functor), long, std::string>(
+            "time", functor);
+    fmt::print("df_24h size {}\n", df_24h.get_index().size());
+    if (df_24h.get_index().size()) {
+        df_24h.template write<std::ostream, int, long, double, std::string>(
+            std::cout);
+        ModeVisitor<2, std::string> mode_visit;
+        const auto&                 result =
+            df_24h.template single_act_visit<std::string>("pkg", mode_visit)
+                .get_result();
+        std::cout << "topNFeaIn24h:";
+        for (auto& i : result) {
+            std::cout << i.get_value() << ",";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename I, typename H>
+void extractEventNumFea(DataFrame<I, H>& df, long req_ts) {
+    // use
+    auto time_vec = df.template get_column<long>("time");
+    for (auto& i : time_vec) {
+        i = (req_ts - i) / (24 * 3600);
+    }
+    df.template load_column<long>("num_day", time_vec);
+    auto dvec = df.template get_column<long>("num_day");
+    // std::for_each(dvec.begin(), dvec.end(), [](const auto &i){std::cout << i
+    // << " "; });
+
+    auto num_df = df.template groupby1<long>(
+        "num_day",
+        LastVisitor<MyDataFrame::IndexType, MyDataFrame::IndexType>(),
+        std::make_tuple("num_day", "event_num_day", CountVisitor<long>()));
+    // num_df.template write<std::ostream, long, std::string>(std::cout,
+    // io_format::csv2);
+    auto event_num_day_vec =
+        num_df.template get_column<size_t>("event_num_day");
+    for (int i = 0; i < std::min(7, (int)event_num_day_vec.size()); i++) {
+        fmt::print("event_num_day_{}={},", i + 1, (int)event_num_day_vec[i]);
+    };
+}
+
+template <typename I, typename H>
+void extractSessionFea(DataFrame<I, H>& df, long req_ts) {
+    ReturnVisitor<long> return_visit(return_policy::monetary);
+    // df.template sort<long>("time", sort_spec::ascen);
+    df.template single_act_visit<long>("time", return_visit);
+    df.template load_column<long>("time_diff", return_visit.get_result());
+
+    FactorizeVisitor<long> fact(
+        [](const long& f) -> bool { return (f > -600); });
+    df.template load_column(
+        "time_diff_bool",
+        df.template single_act_visit<long>("time_diff", fact).get_result());
+    auto session_df = df.template groupby1<bool>(
+        "time_diff_bool",
+        LastVisitor<MyDataFrame::IndexType, MyDataFrame::IndexType>(),
+        std::make_tuple("time_diff", "all_session_time", SumVisitor<long>()),
+        std::make_tuple("time_diff", "session_num", CountVisitor<long>()));
+    // session_df.template write<std::ostream, int, long, bool, size_t,
+    // std::string>(std::cout, io_format::csv2);
+
+    auto event_list = session_df.template get_column<bool>("time_diff_bool");
+    auto event_num = (long)event_list.size();
+    auto session_num_14d =
+        session_df.template get_column<size_t>("session_num")[0];
+    auto all_session_time =
+        session_df.template get_column<long>("all_session_time")[1];
+    auto avg_session_minute = abs(all_session_time) / 60 / session_num_14d;
+    auto avg_imp_in_session = (float)event_num / session_num_14d;
+    // latest_session_time decided by time_diff between req_ts and cur_ts
+    // auto latest_session_time = session_df.template get_column<long>
+    // ("time_diff")[0];
+
+    fmt::print("session_num={}; all_session_time={}, avg_session_minute={}, "
+               "avg_imp_in_session={} \n",
+               session_num_14d,
+               all_session_time,
+               avg_session_minute,
+               avg_imp_in_session);
+}
+// click funcs
+template <typename I, typename H>
+void extractImpNotCli(DataFrame<I, H>& df_clk, DataFrame<I, H>& df_imp) {
+    // calculate imp_not_click_pkgs
+    std::cout << "in func: extractImpNotCli"
+              << "\n";
+    // df_clk.template write<std::ostream, long, std::string>(std::cout,
+    // io_format::csv2); problem1: segment fault
+    auto cli_pkg_vec = df_clk.template get_column<std::string>("pkg");
+    // std::for_each(cli_pkg_vec.begin(), cli_pkg_vec.end(), [](const auto
+    // &i){std::cout << i << " "; });
+    auto imp_pkg_vec = df_imp.template get_column<std::string>("pkg");
+    for (auto it = imp_pkg_vec.begin(); it != imp_pkg_vec.end(); it++) {
+        if (std::find(cli_pkg_vec.begin(), cli_pkg_vec.end(), *it) !=
+            cli_pkg_vec.end()) {
+            imp_pkg_vec.erase(it);
+        }
+    }
+    std::cout << "imp_not_click:";
+    for (auto& i : imp_pkg_vec) {
+        std::cout << i << ",";
+    }
+    std::cout << "\n";
+}
+
+template <typename I, typename H>
+void extractCtTopN(DataFrame<I, H>& df) {
+    auto time_vec = df.template get_column<long>("time");
+    for (auto& i : time_vec) {
+        i = (i / 3600) % 24;
+    }
+    df.template load_column<long>("hour_of_day", time_vec);
+    ModeVisitor<1, long> mode_visit;
+    const auto&          result =
+        df.template single_act_visit<long>("hour_of_day", mode_visit)
+            .get_result();
+    std::cout << "ctTop3:";
+    for (auto& i : result) {
+        std::cout << i.get_value() << ",";
+    }
+    std::cout << "\n";
+}
+
+template <typename I, typename H>
+int CalcImpUpsFea(DataFrame<I, H>& df) {
+    // check time column
+    auto dvec = df.template get_column<std::string>("time");
+    std::for_each(
+        dvec.begin(), dvec.end(), [](const auto& i) { std::cout << i << " "; });
+    std::cout << "\n";
+
+    const auto p1 = std::chrono::system_clock::now();
+    long       req_ts = (long)std::chrono::duration_cast<std::chrono::seconds>(
+                      p1.time_since_epoch())
+                      .count();
+    req_ts = 1622736000;
+
+    df.template retype_column<std::string, long>(
+        "time",
+        [](const std::string& val) -> long { return (std::stol(val)); });
+
+    extractSessionFea(df, req_ts);
+    extractEventNumFea(df, req_ts);
+    auto imp_pkg_num = (int)dvec.size();
+    fmt::print("imp_pkg_num={}\n", imp_pkg_num);
+
+    return 0;
+}
+
+template <typename I, typename H>
+int CalcInsUpsFea(DataFrame<I, H>& df_ins) {
+    const auto p1 = std::chrono::system_clock::now();
+    long       req_ts = (long)std::chrono::duration_cast<std::chrono::seconds>(
+                      p1.time_since_epoch())
+                      .count();
+    req_ts = 1624118400;
+
+    df_ins.template retype_column<std::string, long>(
+        "time",
+        [](const std::string& val) -> long { return (std::stol(val)); });
+    auto dvec = df_ins.template get_column<long>("time");
+
+    extractTopNFea(df_ins, "Ins");
+    auto df_ins_24h = extractTopNFeaByTime(df_ins, req_ts, "Ins");
+    extractEventNumFea(df_ins, req_ts);
+
+    // extractCtTopN(df_clk);
+    auto ins_pkg_num = (int)dvec.size();
+    fmt::print("ins_pkg_num={}\n", ins_pkg_num);
+    auto pkg_vec = df_ins.template get_column<std::string>("pkg");
+    auto last_ip = pkg_vec[0];
+    fmt::print("last_ip={}\n", last_ip);
+    unsigned long last_ip_time_diff = req_ts - dvec[0];
+    fmt::print("last_ip_time_diff={}\n", last_ip_time_diff);
+    return 0;
+}
+
+template <typename I, typename H>
+int CalcCliUpsFea(DataFrame<I, H>& df_clk, DataFrame<I, H>& df_imp) {
+    // check time column
+    auto dvec = df_clk.template get_column<std::string>("time");
+
+    const auto p1 = std::chrono::system_clock::now();
+    long       req_ts = (long)std::chrono::duration_cast<std::chrono::seconds>(
+                      p1.time_since_epoch())
+                      .count();
+    req_ts = 1624118400;
+
+    df_clk.template retype_column<std::string, long>(
+        "time",
+        [](const std::string& val) -> long { return (std::stol(val)); });
+    // problem1: mode_visit直接传3会有问题
+    extractTopNFea(df_clk, "Cli");
+    auto df_clk_24h = extractTopNFeaByTime(df_clk, req_ts, "Cli");
+    if (dvec.size() > 3) {
+        extractEventNumFea(df_clk, req_ts);
+        extractCtTopN(df_clk);
+        // problem2: 某几条数据在取pkg时会segment fault
+        // extractImpNotCli(df_clk, df_imp);
+        auto clk_pkg_num = (int)dvec.size();
+        fmt::print("clk_pkg_num={}\n", clk_pkg_num);
+        // pkg 24h uniq
+        auto cli_pkg_vec_24h =
+            df_clk_24h.template get_col_unique_values<std::string>("pkg");
+        std::cout << "pkg_seq_24_hour_uniq:";
+        for (auto& i : cli_pkg_vec_24h) {
+            std::cout << i << ",";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
+
 static const char* kTypeNames[] = {
     "Null", "False", "True", "Object", "Array", "String", "Number"};
 template <typename I, typename H>
@@ -164,6 +427,15 @@ int TableToDataFrame(const Table& tlb, DataFrame<I, H>& df) {
     for (auto& iter : ns_visitor.get_result()) {
         nsmall_str.append(fmt::format("{}|{},", iter.index, iter.value));
     }
+    auto thr = 10000;
+    auto fn = [thr](const unsigned long&, const long& val) {
+        return val > thr;
+    };
+    auto df_view =
+        df.template get_view_by_sel<long, decltype(fn), long, std::string>(
+            "appid", fn);
+    df_view.template write<std::ostream, int, long, double, std::string>(
+        std::cout);
     fmt::print("shape {}:{} column_value {} {} and "
                "sum()={}\nnlargets={}\nsmallest={}\n",
                df.shape().first,
@@ -188,6 +460,30 @@ int TableToDataFrame(const Table& tlb, DataFrame<I, H>& df) {
     df.template single_act_visit<long>("time", return_visit);
     df.template load_column<long>("time_diff", return_visit.get_result());
     df.template write<std::ostream, int, long, double, std::string>(std::cout);
+
+    // check time column
+    auto dvec = df.template get_column<std::string>("time");
+    std::for_each(
+        dvec.begin(), dvec.end(), [](const auto& i) { std::cout << i << " "; });
+    std::cout << "\n";
+
+    const auto p1 = std::chrono::system_clock::now();
+    long       req_ts = (long)std::chrono::duration_cast<std::chrono::seconds>(
+                      p1.time_since_epoch())
+                      .count();
+    req_ts = 1623159350;
+
+    df.template retype_column<std::string, long>(
+        "time",
+        [](const std::string& val) -> long { return (std::stol(val)); });
+
+    extractTopNFea(df);
+    extractTopNFeaByTime(df, req_ts); // also modeVisitor
+    // extractSessionFea(df, req_ts);
+    // extractEventNumFea(df, req_ts);
+
+    auto imp_pkg_num = (int)dvec.size();
+    fmt::print("imp_pkg_num={}", imp_pkg_num);
 #if 0
     // groupby
     auto ndf = df.template groupby1<std::string>(
